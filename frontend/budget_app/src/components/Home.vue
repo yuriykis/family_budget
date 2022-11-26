dcc593bc532fbc227b9c6a2d09eb2b37f92b41a2
Yuriy Kis <yurakis9@gmail.com>
2022-11-26 18:52:39 +0100

    Create initial home page view
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

---
+++ frontend/budget_app/src/components/Home.vue
@@ -0,0 +1,73 @@
+<template>
+    <v-container>
+        <v-row>
+            <!-- cards with family budget -->
+            <v-card>
+                <v-card-title>
+                    <h3 class="headline mb-0">Family Budget</h3>
+                </v-card-title>
+                <v-card-text>
+                    <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-card class="elevation-12">
+                                <v-toolbar dark flat>
+                                    <v-spacer />
+                                    <v-toolbar-title>Income</v-toolbar-title>
+                                    <v-spacer />
+                                </v-toolbar>
+                                <v-card-text>
+                                    <v-form>
+                                        <v-text-field prepend-icon="mdi-cash" name="income" label="Income" type="text">
+                                        </v-text-field>
+                                    </v-form>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn dark>Save</v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-card class="elevation-12">
+                                <v-toolbar dark flat>
+                                    <v-spacer />
+                                    <v-toolbar-title>Expenses</v-toolbar-title>
+                                    <v-spacer />
+                                </v-toolbar>
+                                <v-card-text>
+                                    <v-form>
+                                        <v-text-field prepend-icon="mdi-cash" name="expenses" label="Expenses"
+                                            type="text"></v-text-field>
+                                    </v-form>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn dark>Save</v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-card class="elevation-12">
+                                <v-toolbar dark flat>
+                                    <v-spacer />
+                                    <v-toolbar-title>Balance</v-toolbar-title>
+                                    <v-spacer />
+                                </v-toolbar>
+                                <v-card-text>
+                                    <v-form>
+                                        <v-text-field prepend-icon="mdi-cash" name="balance" label="Balance"
+                                            type="text"></v-text-field>
+                                    </v-form>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn dark>Save</v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+            </v-card>
+        </v-row>
+    </v-container>
+</template>
