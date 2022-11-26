e5c3c625dc10200686f422e1937a12af6d0c9817
Yuriy Kis <yurakis9@gmail.com>
2022-11-26 17:20:55 +0100

    Create initial routes
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

---
+++ frontend/budget_app/src/plugins/router.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue')
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: () => import('../views/About.vue')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import('../views/Login.vue'),
+        meta: {
+            requiresAuth: false
+        }
+    },
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    const loggedIn = localStorage.getItem('user')
+    if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
+        next('/login')
+    } else {
+        next()
+    }
+})
+
+export default router
+
+
