91c5a0d1e5eef82e2469781063be607ecccc2bd0
Yuriy Kis <yurakis9@gmail.com>
2022-11-26 18:55:19 +0100

    Login and About pages
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

--- frontend/budget_app/src/components/Login.vue
+++ frontend/budget_app/src/components/Login.vue
@@ -1,14 +1,12 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="12" sm="8" md="4">
+    <v-row aling="center" justify="center">
+      <v-col cols=" 12" sm="8" md="4">
         <v-card class="elevation-12">
-          <v-toolbar color="primary" dark flat>
-            <v-toolbar-title>Login</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>mdi-magnify</v-icon>
-            </v-btn>
+          <v-toolbar dark flat>
+            <v-spacer />
+            <v-toolbar-title>FamilyBudget</v-toolbar-title>
+            <v-spacer />
           </v-toolbar>
           <v-card-text>
             <v-form>
@@ -19,7 +17,7 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary">Login</v-btn>
+            <v-btn dark>Login</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
