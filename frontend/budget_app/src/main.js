09145f978a6481b5f97e929f3747e4e781ddbc1d
Yuriy Kis <yurakis9@gmail.com>
2022-11-26 18:54:20 +0100

    Connect plugins to app
    Connect vuetify, router and vuex store
    
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

--- frontend/budget_app/src/main.js
+++ frontend/budget_app/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './components/App.vue'
 import vuetify from './plugins/vuetify'
+import router from './plugins/router'
+import store from './store'
 
 Vue.config.productionTip = false
 
+
 new Vue({
   vuetify,
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
