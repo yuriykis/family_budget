f0d16a46b85ba6ac4dbcda41a2d825dcafc16669
Yuriy Kis <yurakis9@gmail.com>
2022-11-27 18:08:40 +0100

    Init django models
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

--- app/budget_app/models.py
+++ app/budget_app/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Budget(models.Model):
+    """Main budget model"""
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+    class Meta:
+        """Order by created_at"""
+
+        ordering = ["-created_at"]
+
+
+class Category(models.Model):
+    """Category model"""
+
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+    class Meta:
+        """Order by name"""
+
+        ordering = ["-name"]
+
+
+class Transaction(models.Model):
+    """Transaction model"""
+
+    class TransactionType(models.TextChoices):
+        """Transaction type choices"""
+
+        INCOME = "INCOME", "Income"
+        EXPENSE = "EXPENSE", "Expense"
+
+    title = models.CharField(max_length=100)
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(
+        max_length=7, choices=TransactionType.choices, default=TransactionType.EXPENSE
+    )
+    datetime = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.title)
+
+    class Meta:
+        """Order by created_at"""
+
+        ordering = ["-created_at"]
+
+
+class UserBudget(models.Model):
+    """User budget model"""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    ownership = models.BooleanField(default=False)
+    readonly = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.user)
+
+    class Meta:
+        """Order by created_at"""
+
+        ordering = ["-created_at"]
